9d736716a776cb909d58afd1cae1c9ae53220b34
LKubner <luciano.2022310952@aluno.iffar.edu.br>
2024-11-29 09:22:48 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="pt-br" xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+    <style type="text/css">
+p{color:black;}
+
+.container {
+    width:70%;
+
+}
+body{
+
+    justify-content: center;
+    margin: 0;
+    display: flex;
+    font-family: Arial;
+}
+.header{
+    background-color: lightcoral;
+    text-align: center;
+    padding: 10px, 0;
+    height: 100px;
+   
+}
+h1 {
+    padding: 30px 0;
+    text-align: center;
+    color:white;
+    font-family: Arial;
+    margin-top: 0;
+}
+.p1 {
+    text-align: center;
+    font-family: Arial;
+}
+h2 {
+    background-color: lightpink;
+    margin-top: 0;
+    padding: 10px;
+    font-family: Arial;
+    border-width: 0px 0px 3px 0px;
+    border-style: solid;
+    border-color: purple;
+}
+a {
+    color:lightcoral;
+}
+
+
+</style>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+<div class="container">
+
+<div class="header">
+        <h1>Fulano da Silva</h1> </div>
+
+        <p class="p1">Nacionalidade brasileira, 20 anos, casado, natural da cidade de Uruguaiana.</p>
+
+        <h2>Experiência profissional</h2>
+        <h3><a href="http://uol.com.br" target="_blank">Farmácias Panvel</a>  2 meses</h3>
+        <h4>Analista Programador - 10 meses</h4>
+        <p>Desenvolvimento de aplicações web com HTML, CSS, Javascript e PHP.</p>
+
+        <h3><a href="http://terra.com.br" target="_blank"> Rede Globo</a> - 1 ano</h3>
+        <h4>Desenvolvedor Front End, 1 ano</h4>
+        <p>Desenvolvedor Front-End (aplicação WEB e mobile).</p>
+        <p>Utilização de tecnologias como HTML5, CSS, Wordpress, PHP e Javascript.</p>
+
+        <h3><a href="http://g1.com" target="_blank">Google</a> - 6 meses</h3>
+        <h4>Desenvolvedor web, 6 meses</h4>
+        <p>Integração de inteligência artificial de recomendações, vitrine e funções para a loja em Ecommerce e desenvolvimento de aplicações.</p>
+
+        <h2>Formação</h2>
+        <h3>Graduação em Tecnologia da Informação</h3>
+        <p class="destaque">01/2025 a 12/2027</p>
+        <p>FIAP - Faculdade de Tecnologia e Administração Paulista</p>
+
+        <h3>Pós-graduação em sistemas inteligentes</h3>
+        <p class="destaque">08/2028 a 12/2029</p>
+        <p>UFRGS</p>
+
+        <h2>Cursos / Especializações</h2>
+        <h3>Programação Swift 2</h3>
+        <p>Máxima informática 90h, 2028</p>
+
+        <h3>Bootstrap</h3>
+        <p>InfoVida 95h - 2025</p>
+
+        <h3>CSS3 e sites responsivos</h3>
+        <p>InfoVida 50h - 2026</p>
+
+        <h2>Idiomas</h2>
+        <h3>Inglês</h3>
+        <p>Nível avançado</p>
+        <h3>Espanhol</h3>
+        <p>Nível intermediário</p>
+
+        <h2>Informações adicionais</h2>
+        <p>Possui um Endereço alternativo para trabalhos na cidade de São Paulo, próximo a Estação Lago Azul.
+        Endereço: Estância Lago Negro - RS.</p>
+    </body>
+</div>
+</html>
